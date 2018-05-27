0f4cf27b356f990a4e3eb6c6e3c9e3f3b03989c7
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-05-27 09:41:01 -0300

    Limits stored in variables

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int min, max;
 
     // Use this for initialization
     void Start()
     {
+        min = 1;
+        max = 1000;
+
         Debug.Log("Welcome to Number Wizard, hooman!");
-        Debug.Log("Think of a number between 1 and 1000, hooman.");
+        Debug.Log("Think of a number between " + min + " and " + max + ", hooman.");
         Debug.Log("But don't tell me! Now let's play...");
     }
 
