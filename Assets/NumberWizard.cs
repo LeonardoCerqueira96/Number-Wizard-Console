5f047a4bf69c5dd718387f556dcbf1452f2ae867
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-05-27 09:21:21 -0300

    Startup messages written.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard, hooman!");
+        Debug.Log("Think of a number between 1 and 1000, hooman.");
+        Debug.Log("But don't tell me! Now let's play...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
